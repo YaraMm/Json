05e4d36bfecd470a4df8e1b4d037360ee548ffd8
YaraMm <for.steam2016@mail.ru>
2022-03-11 19:57:41 +0400

    Учу Json!

---
+++ TaskJson/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace TaskJson
+{
+    internal class Program
+    {
+        public class tests
+        {
+            public int id { get; set; }
+            public string title { get; set; }
+            public string values { get; set; }
+        }
+        public class values
+        {
+            public int id { get; set; }
+            public string value { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            string testsF = File.ReadAllText("tests.json");
+            var testsJ = JsonConvert.DeserializeObject<tests[]>(testsF);
+
+            string valuesF = File.ReadAllText("values.json");
+            var valuesJ = JsonConvert.DeserializeObject<values[]>(valuesF);
+
+
+            //testsJ.Where(x => x.id == 41).First().id = ;
+
+            //Console.WriteLine(testsJ);
+
+        }
+    }
+}
